c7f209c0e5efc8692f0c7715ef733222a47535ce
TobiasLuna22 <tobiasluna.eco@gmail.com>
2024-12-21 22:43:43 -0300

    items

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
     <div className="App">
-      <NavBar/>
-      <ItemListContainer greeting={"Bienvenidos"}/>
-      <ItemDetailContainer/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+          <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
-    </>
   );
 }
 
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,23 +2,30 @@ import { useState, useEffect } from "react";
 import { getProducts } from "../../asyncMock";
 import ItemList from "../ItemList/ItemList";
 
-const ItemListContainer = ({ greeting }) => {
-    const [ products, setProducts ] = useState([])
+import { useParams } from "react-router-dom"
 
-    useEffect ( () => {
-        getProducts()
-        .then(response => {
-            setProducts(response)
-        })
-        .catch(error => {
-            console.error(error)
-        })
-    },[])
+const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+
+    const { categoryId } = useParams()
+
+    useEffect(() => {
+        const asyncFunc = categoryId ? getProductsByCategory : getProducts
+
+        asyncFunc(categoryId)
+            .then(response => {
+                setProducts(response)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }, [categoryId]);
+
 
     return (
         <div>
             <h1>{greeting}</h1>
-            <ItemList products={products}/>
+            <ItemList products={products} />
         </div>
     )
 }
